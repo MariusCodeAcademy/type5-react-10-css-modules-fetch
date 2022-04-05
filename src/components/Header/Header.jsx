4ada72536253b9704b893de245f53ddb3773aca1
Marius <marius@example.com>
2022-04-05 16:36:12 +0300

    header css ok

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,20 @@
+import css from './Header.module.css';
 // isitraukti module.css
 // sudeti kur reikia klases headeriui, nav, a
+
 const Header = () => {
   return (
-    <header>
-      <nav>
-        <a href='#home'>Home</a>
-        <a href='#about'>About</a>
-        <a href='#contact'>Contact</a>
+    <header className={css.header}>
+      <nav className={css.nav}>
+        <a className={css.navLink} href='#home'>
+          Home
+        </a>
+        <a className={css.navLink} href='#about'>
+          About
+        </a>
+        <a className={css.navLink} href='#contact'>
+          Contact
+        </a>
       </nav>
     </header>
   );
---
+++ src/components/IconCardSection/IconCardSection.jsx
@@ -0,0 +1,13 @@
+const IconCardSection = () => {
+  return (
+    <section>
+      <div className='container'>
+        <h2>Here's all the stuff I do.</h2>
+        <p>Odio turpis amet sed consequat eget posuere consequat.</p>
+        <div className='icon-cards'>icon cards</div>
+        <p>Lorem ipsum dolor sit sapien vestibulum ipsum primis?</p>
+        <button>See some of my recent work</button>
+      </div>
+    </section>
+  );
+};
