f163133ed1d9071f92ddd93c464b7835efb8f220
Marius <marius@example.com>
2022-04-05 16:14:11 +0300

    module

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,15 +3,15 @@ import './card.css';
 const Card = () => {
   const titleStyle = {
     textTransform: 'uppercase',
-    'font-size': '3rem',
+    fontSize: '3rem',
   };
 
   return (
-    <div className='card'>
+    <div className='card border'>
       <h2 style={titleStyle} className='title'>
         Card title
       </h2>
-      <p className='text'>
+      <p className='card__text'>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit, modi.
       </p>
       <button className='btn'>Click me</button>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,14 @@
-import './post.css';
+import css from './Post.module.css';
+
+console.log('css ===', css);
 
 const Post = () => (
-  <div className='post'>
-    <h2 className='title'>Post title</h2>
-    <p className='text'>
+  <div className={`border ${css.post}`}>
+    <h2 className={css.title}>Post title</h2>
+    <p className={css.text}>
       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit, modi.
     </p>
-    <button className='btn'>Learn more</button>
+    <button className={css.btn}>Learn more</button>
   </div>
 );
 
